02b494b67f212c5019a18622b7526ad7b75ece00
talbot648 <charlietalbot648gmail.com>
2024-10-24 15:16:26 +0100

    refactored face left and right to use a map

--- marsRover.go
+++ marsRover.go
@@ -19,45 +19,26 @@ func (r *Rover) GetPosition() (int, int, string) {
 	return r.x, r.y, r.direction
 }
 
-// refactor later to array
 func (r *Rover) FaceLeft() {
-	if r.direction == "N" {
-		r.direction = "W"
-	} else if r.direction == "W" {
-		r.direction = "S"
-	} else if r.direction == "S" {
-		r.direction = "E"
-	} else if r.direction == "E" {
-		r.direction = "N"
+	directionMap := map[string]string{
+		"N": "W",
+		"W": "S",
+		"S": "E",
+		"E": "N",
 	}
+	r.direction = directionMap[r.direction]
 }
 
 func (r *Rover) FaceRight() {
-	if r.direction == "N" {
-		r.direction = "E"
-	} else if r.direction == "E" {
-		r.direction = "S"
-	} else if r.direction == "S" {
-		r.direction = "W"
-	} else if r.direction == "W" {
-		r.direction = "N"
+	directionMap := map[string]string{
+		"N": "E",
+		"W": "N",
+		"S": "W",
+		"E": "S",
 	}
+	r.direction = directionMap[r.direction]
 }
 
-/*
-	func (r *Rover) MoveForward() {
-		switch r.direction {
-		case "N":
-			r.y = (r.y + 1) % gridSize
-		case "E":
-			r.x = (r.x + 1) % gridSize
-		case "S":
-			r.y = (r.y - 1 + gridSize) % gridSize
-		case "W":
-			r.x = (r.x - 1 + gridSize) % gridSize
-		}
-	}
-*/
 func (r *Rover) MoveForward() {
 	r.Move(1)
 }
