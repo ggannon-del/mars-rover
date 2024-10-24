5bbb189924a8e8b9f73952016354a4db7aebf5ba
katieEliz <katie.morton@bjss.com>
2024-10-24 12:47:55 +0100

    added turnLeft func and test

--- marsRover.go
+++ marsRover.go
@@ -1,16 +1,30 @@
 package main
 
 type Rover struct {
-	x, y int
+	x, y      int
+	direction string
 }
 
-func NewRover(x, y int) *Rover {
+func NewRover(x, y int, direction string) *Rover {
 	return &Rover{
-		x: x,
-		y: y,
+		x:         x,
+		y:         y,
+		direction: direction,
 	}
 }
 
-func (r *Rover) GetPosition() (int, int) {
-	return r.x, r.y
+func (r *Rover) GetPosition() (int, int, string) {
+	return r.x, r.y, r.direction
+}
+
+func (r *Rover) FaceLeft() {
+	if r.direction == "N" {
+		r.direction = "W"
+	} else if r.direction == "W" {
+		r.direction = "S"
+	} else if r.direction == "S" {
+		r.direction = "E"
+	} else if r.direction == "E" {
+		r.direction = "N"
+	}
 }
--- mr_test.go
+++ mr_test.go
@@ -5,11 +5,23 @@ import (
 )
 
 func TestRoversInlitilisation(t *testing.T) {
-	Rover := NewRover(0, 0)
+	Rover := NewRover(0, 0, "N")
 
-	x, y := Rover.GetPosition()
+	x, y, _ := Rover.GetPosition()
 
 	if x != 0 || y != 0 {
 		t.Error("Expected position (0,0)", x, y)
+	}
+}
+
+func TestFaceLeft(t *testing.T) {
+	Rover := NewRover(0, 0, "N")
+
+	Rover.FaceLeft()
+
+	_, _, direction := Rover.GetPosition()
+
+	if direction != "W" {
+		t.Error("Expected direction W after turning left")
 	}
 }
